0bacb98e4e8decdff19c438f27f6e616418f28bd
povser75@outlook.com <povser75@outlook.com>
2024-02-08 19:34:55 +0400

    Изменение шрифта и цвета надписи

--- Apteka_Plus/DisignUserControl.Designer.cs
+++ Apteka_Plus/DisignUserControl.Designer.cs
@@ -31,8 +31,18 @@
             this.label1 = new System.Windows.Forms.Label();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.SampleLabel = new System.Windows.Forms.Label();
+            this.EditLabelBTN = new System.Windows.Forms.Button();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.SamplePanel = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
             this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -48,6 +58,8 @@
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
             this.tabControl1.Location = new System.Drawing.Point(5, 28);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
@@ -56,6 +68,8 @@
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.SampleLabel);
+            this.tabPage1.Controls.Add(this.EditLabelBTN);
             this.tabPage1.Location = new System.Drawing.Point(4, 34);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
@@ -64,15 +78,84 @@
             this.tabPage1.Text = "Дизайн Надписей";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
+            // SampleLabel
+            // 
+            this.SampleLabel.AutoSize = true;
+            this.SampleLabel.Location = new System.Drawing.Point(374, 120);
+            this.SampleLabel.Name = "SampleLabel";
+            this.SampleLabel.Size = new System.Drawing.Size(174, 25);
+            this.SampleLabel.TabIndex = 1;
+            this.SampleLabel.Text = "Образец надписи";
+            // 
+            // EditLabelBTN
+            // 
+            this.EditLabelBTN.Location = new System.Drawing.Point(133, 120);
+            this.EditLabelBTN.Name = "EditLabelBTN";
+            this.EditLabelBTN.Size = new System.Drawing.Size(206, 45);
+            this.EditLabelBTN.TabIndex = 0;
+            this.EditLabelBTN.Text = "Изменить надпись";
+            this.EditLabelBTN.UseVisualStyleBackColor = true;
+            this.EditLabelBTN.Click += new System.EventHandler(this.EditLabelBTN_Click);
+            // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.SamplePanel);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.button2);
             this.tabPage2.Location = new System.Drawing.Point(4, 34);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(900, 446);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Дизайн Кнопок";
+            this.tabPage2.Text = "Дизайн Панелей";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // SamplePanel
+            // 
+            this.SamplePanel.Location = new System.Drawing.Point(383, 106);
+            this.SamplePanel.Name = "SamplePanel";
+            this.SamplePanel.Size = new System.Drawing.Size(393, 270);
+            this.SamplePanel.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(378, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(164, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Образец панели";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(143, 106);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(206, 45);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Изменить панель";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 34);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(900, 446);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Дизайн ТекстБоксов";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 34);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(900, 446);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Дизайн Кнопок";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // fontDialog1
+            // 
+            this.fontDialog1.ShowColor = true;
             // 
             // DisignUserControl
             // 
@@ -85,6 +168,10 @@
             this.Name = "DisignUserControl";
             this.Size = new System.Drawing.Size(916, 515);
             this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -96,5 +183,13 @@
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label SampleLabel;
+        private System.Windows.Forms.Button EditLabelBTN;
+        private System.Windows.Forms.Panel SamplePanel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.FontDialog fontDialog1;
     }
 }
--- Apteka_Plus/DisignUserControl.cs
+++ Apteka_Plus/DisignUserControl.cs
@@ -12,9 +12,24 @@ namespace Apteka_Plus
 {
     public partial class DisignUserControl : UserControl
     {
+        Font LABEL_FONT;
+        Color LABEL_COLOR;
+
         public DisignUserControl()
         {
             InitializeComponent();
+        }
+
+        private void EditLabelBTN_Click(object sender, EventArgs e)
+        {
+            if(fontDialog1.ShowDialog() == DialogResult.OK)
+            {
+                LABEL_FONT = fontDialog1.Font;
+                LABEL_COLOR = fontDialog1.Color;
+
+                SampleLabel.Font = LABEL_FONT;
+                SampleLabel.ForeColor = LABEL_COLOR;
+            }
         }
     }
 }
